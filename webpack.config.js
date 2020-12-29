ac732d044e05a65e318c60430add396b16ecc449
Mike <mstirmii@gmail.com>
2020-12-29 15:42:37 -0500

    updates

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path"); // integrated node package
+ 
+const postCSSPlugins = [        //  array of postcss options
+  require("postcss-import"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer"),
+];
+ 
+module.exports = {
+  entry: "./app/assets/scripts/App.js", // starting file
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"), // where bundled.js goes
+  },
+  mode: "development",
+  watch: true, // tell webpack to rebundle on save
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader?url=false",  // ?url - added from lesson 16 note
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {          // this is the bit that is needed 
+                plugins: postCSSPlugins, // still uses same array as taught
+              },                         // but latest version of postcss has
+            },                           // new config methods
+          },
+        ],
+      },
+    ],
+  },
+};
