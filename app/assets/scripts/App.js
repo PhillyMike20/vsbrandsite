92ddd9598e846dae64c03ffa095cf610943a7b07
Mike <mstirmii@gmail.com>
2021-01-22 14:48:59 -0500

    header updates

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,18 @@ import '../styles/styles.css'
 if (module.hot) {
   module.hot.accept()
 }
+
+/* Lesson code */
+function Person (fullName, favColor) {
+  this.name = fullName;
+  this.favoriteColor = favColor;
+  this.greet = function() {
+    console.log("Hello there, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+  }
+}
+
+let mike = new Person("Mike Stirm", "green");
+mike.greet();
+
+let sally = new Person("Sally Stirm", "yellow");
+sally.greet();
