4cdc7d0557fc93c531c1d8e0b58aa372fd52c788
Mike <mstirmii@gmail.com>
2021-01-23 15:10:25 -0500

    Header Mobile Menu complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,9 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept()
 }
 
-/* Lesson code */
-function Person (fullName, favColor) {
-  this.name = fullName;
-  this.favoriteColor = favColor;
-  this.greet = function() {
-    console.log("Hello there, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
-  }
-}
-
-let mike = new Person("Mike Stirm", "green");
-mike.greet();
-
-let sally = new Person("Sally Stirm", "yellow");
-sally.greet();
