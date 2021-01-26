df1bf9f187d6a6b94b9627f4f0fb4c4822a3f795
Mike <mstirmii@gmail.com>
2021-01-26 11:54:32 -0500

    Completed sticky header

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -7,6 +7,7 @@ class StickyHeader {
     this.primaryNav = document.querySelector(".primary-nav")
     this.pageSections = document.querySelectorAll(".page-section")
     this.browserHeight = window.innerHeight
+    this.previousScrollY = window.scrollY
     this.events()
   }
 
@@ -18,6 +19,8 @@ class StickyHeader {
   }
 
   runOnScroll() {
+    this.determineScrollDirection()
+
     if (window.scrollY > 60) {
       this.siteHeader.classList.add("site-header--dark") && this.primaryNav.classList.add("primary-nav--white")
 
@@ -28,9 +31,27 @@ class StickyHeader {
     this.pageSections.forEach(el => this.calcSection(el))
   }
 
+  determineScrollDirection() {
+    if(window.scrollY > this.previousScrollY) {
+      this.scrollDirection = 'down'
+    } else {
+      this.scrollDirection = 'up'
+    }
+    this.previousScrollY = window.scrollY
+  }
+
   calcSection(el) {
-    if (window.scrollY + window.innerHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
-      let scrollPercent = el.getBoundingClientRect().y / window.innerHeight * 100
+    if (window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
+      let scrollPercent = el.getBoundingClientRect().y / this.browserHeight * 100
+      if (scrollPercent < 18 && scrollPercent > -0.1 && this.scrollDirection == 'down' || scrollPercent < 33 && this.scrollDirection == 'up') {
+        let matchingLink = el.getAttribute("data-matching-link")
+        document.querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach(el => el.classList.remove("is-current-link"))
+        document.querySelector(matchingLink).classList.add("is-current-link")
+      }
+
+      else if (scrollPercent > 38 && this.scrollDirection == 'up') {
+        document.querySelector('#about-me-link').classList.remove('is-current-link');
+      }
     }
   }
 }
