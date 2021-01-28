7dbe0460d1eded06105613b8d91d0fc473b0423c
Mike <mstirmii@gmail.com>
2021-01-28 17:02:47 -0500

    further test 1

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -38,7 +38,7 @@ class Modal {
         <div class="bottom-line"></div> 
         <p class="lead2">Here is how you can reach me</p>
         <form
-          method="POST" data-netlify="true"
+          method="POST" name="contact"
         >
         <input type="hidden" name="form-name" value="contact" />
           <div class="text-fields">
